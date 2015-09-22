ff1ddbb79122f81db5684c11e0b22fdb288d22ff
etsuprun <etsuprun@gmail.com>
2015-09-21 22:41:05 -0400

    Update instagram_graph.R

--- instagram_graph.R
+++ instagram_graph.R
@@ -8,14 +8,16 @@ instagram<-
       sep=" ")
   )
 
+daterange<-paste("Data from",min(as.Date(instagram$date)), "to",max(as.Date(instagram$date)))
+
+
 instagram.summary<-
   aggregate(cbind(count = date) ~ wday+hour, 
             data = instagram, 
             FUN = function(x){NROW(x)})
 myplot<-qplot(hour,count,data=instagram.summary,
               color=wday,
-              main="Average Number of Instagram Posts by Day of Week\n
-              Baltimore National Aquarium\n",
+              main=paste("Total Number of Instagram Posts by Day of Week\nBaltimore National Aquarium\n\n",daterange,"\n"),
               xlab="Hour (from 0 to 23)")
 myplot<-myplot+geom_point()+stat_smooth(se=F)
 print(myplot)
